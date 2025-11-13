9f7ae3134f35a7599b6016d908d1b930044f6a01
lizzzbaker <135661708+lizzzbaker@users.noreply.github.com>
2025-11-13 11:26:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// dynamic footer year
 document.getElementById("year").textContent = new Date().getFullYear();
 
-// fade-in animation
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("loaded");
 });
